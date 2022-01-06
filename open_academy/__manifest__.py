c054c39fb770ff75a5464db1d14509693e6ec475
samuel-ciu <53824980+samuel-villa@users.noreply.github.com>
2022-01-06 16:10:53 +0100

    [ADD] dashboard

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -20,7 +20,11 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
+    'depends': [
+        'base', 
+        'sale',
+        'board',
+    ],
 
     # always loaded
     'data': [
